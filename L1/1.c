46dbb4f69f23891cd80f6af19f20a1141df87b8b
Spiryd <neumann.maks@gmail.com>
2023-03-07 11:38:09 +0100

    added stack

--- L1/1.c
+++ L1/1.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
+
+//classic fifo queue
 typedef struct{
     size_t head;
     size_t tail;
     size_t size;
     void** data;
-}fifo_queue;
+}fifo;
 
-void* fifo_read(fifo_queue *queue){
+//read from fifo queue
+void* fifo_read(fifo *queue){
     if(queue->tail == queue->head){
         return NULL;
     }
@@ -18,7 +21,8 @@ void* fifo_read(fifo_queue *queue){
     return handle;
 }
 
-void* fifo_write(fifo_queue *queue, void* handle){
+//write to fifo queue
+void* fifo_write(fifo *queue, void* handle){
     if(((queue->head + 1) % queue->size) ==  queue->tail){
         return -1;
     }
@@ -27,7 +31,63 @@ void* fifo_write(fifo_queue *queue, void* handle){
     return 0;
 }
 
-fifo_queue make_fifo(int size){
-    fifo_queue queue = {0, 0, size, malloc(sizeof(void*) * size)};
+//construckt empty fifo queue
+fifo make_fifo(int size){
+    fifo queue = {0, 0, size, malloc(sizeof(void*) * size)};
     return queue;
+}
+
+//classic stack(FILO)
+typedef struct{
+    size_t top;
+    size_t size;
+    void** data;
+}stack;
+
+//checs if stack is full
+int is_full(stack *st){
+    if(st->top == st->size - 1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+//checs if stack is empty
+int is_empty(stack *st){
+    if(st->top == -1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+//pushes data on top of a stack
+void push(stack *st, void* data){
+    if(is_full(st)){
+        return -1;
+    }else
+    {
+        st->top++;
+        st->data[st->top] = data; 
+    }
+}
+
+//pops off the top of the stack
+void* pop(stack *st){
+    if(is_empty(st)){
+        return NULL;
+    }else
+    {
+        void* data =  st->data[st->top];
+        st->top--;
+        return data;
+    }
+  
+}
+
+//makes an empty stack
+stack make_stack(int size){
+    stack st = {0, size, malloc(sizeof(void*) * size)};
+    return st;
 }
