02d8e8df0713129793771b3be7c499ec5d404781
Spiryd <neumann.maks@gmail.com>
2023-04-24 13:59:48 +0200

    changed folder

--- l3/src/bin/collect_data.rs
+++ l3/src/bin/collect_data.rs
@@ -8,14 +8,14 @@ use std::time::{Instant, Duration};
 
 fn main(){
     let data_thread1 = thread::spawn(|| {
-        let mut file = File::create("select_data.csv").unwrap();
+        let mut file = File::create("./data/select_data.csv").unwrap();
         file.write_all(b"type;n;k;cmps;swaps\n").unwrap();
         let mut stats: (u64, u64);
         let mut list: Vec<u64>;
         let mut line: String;
         for n in 1..=100{
             for k in [0,(n*100)/4, (n*100)/4, (n*300)/4, (n*100 - 1)]{
-                for _ in 0..100 {
+                for _ in 0..1000 {
                     list = gen_list(n*100, Order::Random);
                     stats = select_with_stats(&mut list, k.try_into().unwrap(), 5, false).1;
                     line = format!("select;{};{k};{};{}\n", n*100, stats.0, stats.1);
@@ -25,7 +25,7 @@ fn main(){
         }
         for n in 1..=100{
             for k in [0,(n*100)/4, (n*100)/4, (n*300)/4, (n*100 - 1)]{
-                for _ in 0..100 {
+                for _ in 0..1000 {
                     list = gen_list(n*100, Order::Random);
                     stats = randomized_select_with_stats(&mut list, k.try_into().unwrap(), false).1;
                     line = format!("select;{};{k};{};{}\n", n*100, stats.0, stats.1);
@@ -36,7 +36,7 @@ fn main(){
     });
 
     let data_thread2 = thread::spawn(|| {
-        let mut file = File::create("container_size_data.csv").unwrap();
+        let mut file = File::create("./data/container_size_data.csv").unwrap();
         file.write_all(b"size;n;k;cmps;swaps\n").unwrap();
         let mut stats: (u64, u64);
         let mut list: Vec<u64>;
@@ -44,7 +44,7 @@ fn main(){
         for size in [3, 5, 7, 9] {
             for n in 1..=100{
                 for k in [0,(n*100)/4, (n*100)/4, (n*300)/4, (n*100 - 1)]{
-                    for _ in 0..100 {
+                    for _ in 0..1000 {
                         list = gen_list(n*100, Order::Random);
                         stats = select_with_stats(&mut list, k.try_into().unwrap(), size, false).1;
                         line = format!("{size};{};{k};{};{}\n", n*100, stats.0, stats.1);
@@ -56,7 +56,7 @@ fn main(){
     });
 
     let data_thread3 = thread::spawn(|| {
-        let mut file = File::create("bin_search_data1.csv").unwrap();
+        let mut file = File::create("./data/bin_search_data1.csv").unwrap();
         file.write_all(b"target;n;cmps;time\n").unwrap();
         let mut cmps: u64;
         let mut list: Vec<u64>;
@@ -65,20 +65,53 @@ fn main(){
         let mut time: Duration;
         for n in (1000..=100000).step_by(1000) {
             list = (0..n).collect();
-            for target in [3, n/12, (7 * n)/12, (11 * n)/12, n-4, n]{
-                for _ in 0..100{
-                    start = Instant::now();
-                    cmps = bin_is_in_with_stats(target, list.clone()).1;
-                    time = start.elapsed();
-                    line = format!("{target};{n};{cmps};{}\n", time.as_nanos());
-                    file.write_all(line.as_bytes()).unwrap();
-                }
+            for _ in 0..1000{
+                start = Instant::now();
+                cmps = bin_is_in_with_stats(3, list.clone()).1;
+                time = start.elapsed();
+                line = format!("3;{n};{cmps};{}\n", time.as_nanos());
+                file.write_all(line.as_bytes()).unwrap();
+            }
+            for _ in 0..1000{
+                start = Instant::now();
+                cmps = bin_is_in_with_stats(n/12, list.clone()).1;
+                time = start.elapsed();
+                line = format!("n/12;{n};{cmps};{}\n", time.as_nanos());
+                file.write_all(line.as_bytes()).unwrap();
+            }
+            for _ in 0..1000{
+                start = Instant::now();
+                cmps = bin_is_in_with_stats((7 * n)/12, list.clone()).1;
+                time = start.elapsed();
+                line = format!("7n/12;{n};{cmps};{}\n", time.as_nanos());
+                file.write_all(line.as_bytes()).unwrap();
+            }
+            for _ in 0..1000{
+                start = Instant::now();
+                cmps = bin_is_in_with_stats((11 * n)/12, list.clone()).1;
+                time = start.elapsed();
+                line = format!("11n/12;{n};{cmps};{}\n", time.as_nanos());
+                file.write_all(line.as_bytes()).unwrap();
+            }
+            for _ in 0..1000{
+                start = Instant::now();
+                cmps = bin_is_in_with_stats(n-4, list.clone()).1;
+                time = start.elapsed();
+                line = format!("n-4;{n};{cmps};{}\n", time.as_nanos());
+                file.write_all(line.as_bytes()).unwrap();
+            }
+            for _ in 0..1000{
+                start = Instant::now();
+                cmps = bin_is_in_with_stats(n, list.clone()).1;
+                time = start.elapsed();
+                line = format!("n;{n};{cmps};{}\n", time.as_nanos());
+                file.write_all(line.as_bytes()).unwrap();
             }
         }
     });
 
     let data_thread4 = thread::spawn(|| {
-        let mut file = File::create("bin_search_data2.csv").unwrap();
+        let mut file = File::create("./data/bin_search_data2.csv").unwrap();
         file.write_all(b"n;cmps;time\n").unwrap();
         let mut cmps: u64;
         let mut list: Vec<u64>;
