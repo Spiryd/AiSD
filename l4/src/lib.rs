415a495237ef0f2a93460e80f05b0a4a588726d0
Spiryd <neumann.maks@gmail.com>
2023-06-03 21:05:36 +0200

    implemented final tree structs

--- l4/src/lib.rs
+++ l4/src/lib.rs
@@ -2,6 +2,9 @@ use std::cmp::Ordering;
 use rand::prelude::*;
 use rand_pcg::Pcg64;
 
+static mut CMPS: u32 = 0;
+static mut RNS: u32 = 0;
+
 #[derive(Debug)]
 pub enum Order{
     Random,
@@ -300,6 +303,47 @@ impl RBNode {
         self
     }
 
+    fn find_min_node(&mut self) -> Box<Self> {
+        if let Some(left) = self.left.as_mut() {
+            if left.left.is_none() {
+                return self.left.take().unwrap();
+            }
+            return left.find_min_node();
+        }
+        Box::new(self.clone())
+    }
+
+    fn delete(mut self: Box<Self>, value: u64) -> Option<Box<Self>> {
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                if self.left.is_some() {
+                    let left = self.left.take().unwrap();
+                    self.left = left.delete(value);
+                }
+            }
+            Ordering::Greater => {
+                if self.right.is_some() {
+                    let right = self.right.take().unwrap();
+                    self.right = right.delete(value);
+                }
+            }
+            Ordering::Equal => {
+                if self.right.is_none() {
+                    return self.left;
+                } else if self.left.is_none() {
+                    return self.right;
+                } else {
+                    let mut right = self.right.take().unwrap();
+                    let min_node = right.find_min_node();
+                    self.value = min_node.value;
+                    self.right = right.delete(min_node.value);
+                }
+            }
+        }
+
+        Some(self.balance())
+    }
+
     fn height(&self) -> u64 {
         let left_height = self.left.as_ref().map_or(0, |node| node.height());
         let right_height = self.right.as_ref().map_or(0, |node| node.height());
@@ -349,6 +393,17 @@ impl RBTree {
         self.root.as_mut().unwrap().color = Color::Black;
     }
 
+    pub fn delete(&mut self, value: u64) {
+        if self.root.is_none() {
+            return;
+        }
+        let after_del = self.root.take().unwrap().delete(value);
+        self.root = after_del;
+        if let Some(root) = self.root.as_mut() {
+            root.color = Color::Black;
+        }
+    }
+
     pub fn height(&self) -> u64 {
         self.root.as_ref().map_or(0, |node| node.height())
     }
@@ -358,4 +413,190 @@ impl RBTree {
             root.print_tree_helper("", false);
         }
     }
+}
+
+#[derive(Debug, Clone)]
+struct SplayNode {
+    value: u64,
+    left: Option<Box<SplayNode>>,
+    right: Option<Box<SplayNode>>,
+}
+
+
+impl SplayNode {
+    fn new(value: u64) -> Self {
+        SplayNode {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+
+    // Perform a right rotation on the node.
+    fn rotate_right(mut self: Box<Self>) -> Box<Self> {
+        if self.left.is_some() {
+            let mut x = self.left.take().unwrap();
+            self.left = x.right.take();
+            x.right = Some(self);
+            return x;
+        }
+        self
+    }
+
+    // Perform a left rotation on the node.
+    fn rotate_left(mut self: Box<Self>) -> Box<Self> {
+        if self.right.is_some() {
+            let mut x = self.right.take().unwrap();
+            self.right = x.left.take();
+            x.left = Some(self);
+            return x;
+        }
+        self
+    }
+
+    // Perform a splay operation on the node.
+    fn splay(mut self: Box<Self>, value: u64) -> Box<Self> {
+        if value < self.value {
+            if let Some(ref mut left) = self.left {
+                if value < left.value {
+                    // Zig-Zig
+                    left.left = left.left.take().map(|node| node.splay(value));
+                    self = self.rotate_right();
+                } else if value > left.value {
+                    // Zig-Zag
+                    left.right = left.right.take().map(|node| node.splay(value));
+                    self.left = self.left.map(|node| node.rotate_left());
+                }
+                if let Some(ref mut _left) = self.left {
+                    self = self.rotate_right();
+                }
+            }
+        } else if value > self.value {
+            if let Some(ref mut right) = self.right {
+                if value > right.value {
+                    // Zag-Zag
+                    right.right = right.right.take().map(|node| node.splay(value));
+                    self = self.rotate_left();
+                } else if value < right.value {
+                    // Zag-Zig
+                    right.left = right.left.take().map(|node| node.splay(value));
+                    self.right = self.right.map(|node| node.rotate_right());
+                }
+                if let Some(ref mut _right) = self.right {
+                    self = self.rotate_left();
+                }
+            }
+        }
+        self
+    }
+
+    // Insert a value into the splay tree.
+    fn insert(mut self: Box<Self>, value: u64) -> Box<Self> {
+        if value < self.value {
+            if let Some(left) = self.left.take() {
+                self.left = Some(left.insert(value));
+            } else {
+                self.left = Some(Box::new(SplayNode::new(value)));
+            }
+        } else if value > self.value {
+            if let Some(right) = self.right.take() {
+                self.right = Some(right.insert(value));
+            } else {
+                self.right = Some(Box::new(SplayNode::new(value)));
+            }
+        }
+        self.splay(value)
+    }
+
+    // Perform a delete operation on the node.
+    fn delete(mut self: Box<Self>, value: u64) -> Option<Box<Self>> {
+        self = self.splay(value);
+
+        if value != self.value {
+            return Some(self);
+        }
+
+        match (self.left.take(), self.right.take()) {
+            (None, right) => right,
+            (left, None) => left,
+            (Some(left), Some(right)) => {
+                let mut x = right.splay(value);
+                x.left = Some(left);
+                Some(x)
+            }
+        }
+    }
+
+    // Get the height of the splay tree rooted at this node.
+    fn height(&self) -> u64 {
+        let left_height = self.left.as_ref().map_or(0, |node| node.height());
+        let right_height = self.right.as_ref().map_or(0, |node| node.height());
+        1 + std::cmp::max(left_height, right_height)
+    }
+
+    // Print the node value with the given prefix and side indication.
+    fn print_node(&self, prefix: &str, is_left: bool) {
+        let side = if is_left { "└──" } else { "├──" };
+        println!("{}{} {}", prefix, side, self.value);
+    }
+
+    // Print the splay tree rooted at this node in a readable format.
+    fn print_tree_helper(&self, prefix: &str, is_left: bool) {
+        if let Some(right) = self.right.as_ref() {
+            right.print_tree_helper(&format!("{}{}   ", prefix, if is_left { "│" } else { " " }), false);
+        }
+        self.print_node(prefix, is_left);
+        if let Some(left) = self.left.as_ref() {
+            left.print_tree_helper(&format!("{}{}   ", prefix, if is_left { " " } else { "│" }), true);
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SplayTree {
+    root: Option<Box<SplayNode>>,
+}
+
+impl Default for SplayTree {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SplayTree {
+    pub fn new() -> Self {
+        SplayTree { root: None }
+    }
+
+    pub fn insert(&mut self, value: u64) {
+        if let Some(root) = self.root.take() {
+            self.root = Some(root.insert(value));
+        } else {
+            self.root = Some(Box::new(SplayNode::new(value)));
+        }
+    }
+
+    pub fn height(&self) -> u64 {
+        self.root.as_ref().map_or(0, |node| node.height())
+    }
+
+    pub fn print_tree(&self) {
+        if let Some(root) = self.root.as_ref() {
+            root.print_tree_helper("", false);
+        }
+    }
+
+    pub fn delete(&mut self, value: u64) {
+        if let Some(root) = self.root.take() {
+            self.root = root.delete(value);
+        }
+    }
+}
+
+pub fn get_cmps() -> u32 {
+    unsafe { CMPS }
+}
+
+pub fn get_reads_and_swaps() -> u32 {
+    unsafe { RNS }
 }
--- l4/src/main.rs
+++ l4/src/main.rs
@@ -1,18 +1,129 @@
+use dialoguer::{console::Term, theme::ColorfulTheme, Select};
 use lib::*;
 
 fn main() {
+
+    loop {
+        let choices = ["Test BST", "Test RBTree", "Test SplayTree", "Collect Data", "Exit"];
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("SELECT MODE")
+            .items(&choices)
+            .default(0)
+            .interact_on_opt(&Term::stderr())
+            .expect("failed");
+        if selection.is_none() {
+            panic!("User did not select anything")
+        }
+    
+        match selection.unwrap() {
+            0 => test_bst(),
+            1 => test_rb(),
+            2 => test_splay(),
+            3 => collect_data(),
+            _ => break,
+        }  
+    }
+}
+
+fn test_bst() {
+    let items = gen_list(50, Order::Sorted);
     let mut b_tree = BinarySearchTree::new();
+    for item in &items{
+        println!("Inserting: {}", item);
+        b_tree.insert(*item);
+        b_tree.print();
+        println!("Tree height: {}", b_tree.height());
+    }
     let items = gen_list(50, Order::Random);
     for item in &items{
-        b_tree.insert(*item);
+        println!("Deleting: {}", item);
+        b_tree.delete(*item);
+        b_tree.print();
+        println!("Tree height: {}", b_tree.height());
     }
-    b_tree.print();
-    println!("Tree height: {}", b_tree.height());
+    let items = gen_list(50, Order::Random);
+    let mut b_tree = BinarySearchTree::new();
+    for item in &items{
+        println!("Inserting: {}", item);
+        b_tree.insert(*item);
+        b_tree.print();
+        println!("Tree height: {}", b_tree.height());
+    }
+    let items = gen_list(50, Order::Random);
+    for item in &items{
+        println!("Deleting: {}", item);
+        b_tree.delete(*item);
+        b_tree.print();
+        println!("Tree height: {}", b_tree.height());
+    }
+}
 
+fn test_rb() {
+    let items = gen_list(50, Order::Sorted);
     let mut rb_tree = RBTree::new();
     for item in &items{
+        println!("Inserting: {}", item);
         rb_tree.insert(*item);
+        rb_tree.print_tree();
+        println!("Tree height: {}", rb_tree.height());
     }
-    rb_tree.print_tree();
-    println!("Tree height: {}", rb_tree.height());
+    let items = gen_list(50, Order::Random);
+    for item in &items{
+        println!("Deleting: {}", item);
+        rb_tree.delete(*item);
+        rb_tree.print_tree();
+        println!("Tree height: {}", rb_tree.height());
+    }
+    let items = gen_list(50, Order::Random);
+    let mut rb_tree = RBTree::new();
+    for item in &items{
+        println!("Inserting: {}", item);
+        rb_tree.insert(*item);
+        rb_tree.print_tree();
+        println!("Tree height: {}", rb_tree.height());
+    }
+    let items = gen_list(50, Order::Random);
+    for item in &items{
+        println!("Deleting: {}", item);
+        rb_tree.delete(*item);
+        rb_tree.print_tree();
+        println!("Tree height: {}", rb_tree.height());
+    }
+}
+
+fn test_splay() {
+    let items = gen_list(50, Order::Sorted);
+    let mut splay_tree = SplayTree::new();
+    for item in &items{
+        println!("Inserting: {}", item);
+        splay_tree.insert(*item);
+        splay_tree.print_tree();
+        println!("Tree height: {}", splay_tree.height());
+    }
+    let items = gen_list(50, Order::Random);
+    for item in &items{
+        println!("Deleting: {}", item);
+        splay_tree.delete(*item);
+        splay_tree.print_tree();
+        println!("Tree height: {}", splay_tree.height());
+    }
+    let items = gen_list(50, Order::Random);
+    let mut splay_tree = SplayTree::new();
+    for item in &items{
+        println!("Inserting: {}", item);
+        splay_tree.insert(*item);
+        splay_tree.print_tree();
+        println!("Tree height: {}", splay_tree.height());
+    }
+    let items = gen_list(50, Order::Random);
+    for item in &items{
+        println!("Deleting: {}", item);
+        splay_tree.delete(*item);
+        splay_tree.print_tree();
+        println!("Tree height: {}", splay_tree.height());
+    }
+}
+
+fn collect_data() {
+    get_cmps();
 }
